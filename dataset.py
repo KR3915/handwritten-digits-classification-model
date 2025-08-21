a0731d2127353c688fc23b8804078ad067bf6f39
kr3915 <ondrej.brdek@gmail.com>
2025-08-21 13:30:14 +0200

    datase processing

---
+++ dataset.py
@@ -0,0 +1,20 @@
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    transforms.Resize((28,28)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,),(0.5,))
+])
+dataset = datasets.ImageFolder(root="./dataset/dataset", transform=transform)
+train_size = int(0.8 * len(dataset))
+val_size = len(dataset) - train_size
+train_dataset, val_dataset = torch.utils.data.random_split(dataset,[train_size,val_size])
+
+# data loader
+train_loader = DataLoader(train_dataset, batch_size = 64, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+
+
+
